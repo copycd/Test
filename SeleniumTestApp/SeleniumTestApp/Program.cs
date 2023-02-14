9c1888949b515226b3c7106ab0c41047255a3336
copycd <copycd@daum.net>
2023-02-14 14:53:30 +0900

    .

---
+++ SeleniumTestApp/SeleniumTestApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium.Chrome;
+
+Console.WriteLine("Hello, World!");
+
+
+ChromeDriverService svc = ChromeDriverService.CreateDefaultService();
+var chromeDrv = new ChromeDriver(svc);
+chromeDrv.Navigate().GoToUrl("https://www.naver.com");
